8fdf1860c9de8d26d428d7d79473e376f0311d51
Tom Passarelli <tom.passarelli@protonmail.com>
2023-01-04 23:37:42 -0500

    rb tree test placeholder

---
+++ tests/red-black-tree.spec.js
@@ -0,0 +1,48 @@
+const { Stack } = require('../data-structures/stack') 
+const { expect } = require('chai')
+
+// describe probably verbose, idk
+describe('stack', () => {
+  it('insertion works as expected when 0 el exists', () => {
+    const stck = new Stack()
+    stck.push(1)
+    //let result = stck.peek()
+    let result = stck.first.value
+    // mocha: assert.equal(result, 'mocha test')
+    // chai: expect(result).to.be.eq('mocha test')
+    expect(result).to.be.eq(1)
+  })
+  it('insertion works as expected when 1 el exists', () => {
+    const stck = new Stack()
+    stck.push(2)
+    stck.push(1)
+    //let result = stck.peek()
+    let result = stck.first.value
+    expect(result).to.be.eq(1)
+  })
+  it('pop returns top node', () => {
+    const stck = new Stack()
+    stck.push(2)
+    stck.push(1)
+    let result = stck.pop()
+    expect(result).to.be.eq(1)
+  })
+  it('pop mutates stack to be 1 less.. stack size 2', () => {
+    const stck = new Stack()
+    stck.push(2)
+    stck.push(1)
+    stck.pop()
+    let result = stck.size;
+    expect(result).to.be.eq(1)
+  })
+  it('pop resets first correctly.. stack size 3', () => {
+    const stck = new Stack()
+    stck.push(3)
+    stck.push(2)
+    stck.push(1)
+    stck.pop()
+    //let result = stck.peek()
+    let result = stck.first.value;
+    expect(result).to.be.eq(2)
+  })
+})
